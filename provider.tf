0e73ef3326c69d4b325c902137f5e80b22482db0
itsukinino <itsuki9978miya@gmail.com>
2026-01-02 15:37:54 +0900

    l

--- provider.tf
+++ provider.tf
@@ -1,4 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "ap-northeast-1"
+  alias  = "org"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "identity"
+  region = "us-east-1"
 }
 
